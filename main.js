09f7912ff223e965e4c0e9f00e19a9d003f79a9b
nurik <sapyev-n@>
2024-02-02 03:04:10 +0600

    калькулятордун стили бутту

---
+++ main.js
@@ -0,0 +1,28 @@
+let a = +prompt("Введите первое число");
+let b = +prompt("Введите второе число");
+let c = prompt("Введите значение ' + ' - ' * ' / ' % '");
+if (c == "+") {
+  console.log(a + b);
+}
+if (c == "-") {
+  console.log(a - b);
+}
+if (c == "*") {
+  console.log(a * b);
+}
+if (c == "/") {
+  console.log(a / b);
+}
+if (c == "%") {
+  console.log(a % b);
+}
+//? ----------------------------------------------------------------
+
+function myFunction() {
+  let element = document.getElementById("myCalculator");
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
